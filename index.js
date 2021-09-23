3538f2c6d17214369ce4216ea5abca321d01e03d
Viviana Abigail Cruz <cruzabi994@gmail.com>
2021-09-23 19:49:27 -0300

    ..

--- index.js
+++ index.js
@@ -20,12 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 //¿¿¿
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 // Syncing all the models at once.
 conn.sync({ force: false}).then(() => {
-  server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
 //SET CLIENT_ENCODING TO 'utf8';
